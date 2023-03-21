2d18a18efaa666a669b0383fe296f245adfafe09
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2023-03-21 16:58:32 +0700

    migrateFile

--- A-Buddy/ContentView.swift
+++ A-Buddy/ContentView.swift
@@ -1,88 +1,84 @@
 //
 //  ContentView.swift
-//  A-Buddy
+//  Abuddy
 //
-//  Created by Ahmad Fariz on 20/03/23.
+//  Created by Yovita Handayiani on 20/03/23.
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack(){
+            ZStack(alignment: .trailing){
+                Image("bgfayfay").ignoresSafeArea()
+                Image("QRSymbol").padding(EdgeInsets(top: -130, leading: -80, bottom: 0, trailing: 0))
+            }
+            HStack{
+                Image("profile_fayfay").resizable().frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.gray, lineWidth: 0)
+                    }.shadow(color: Color(red: 220/255, green: 220/255, blue: 220/255), radius: 10,x: 0, y: 3)
+                Text("Unknown")
+            }.offset(x: -100, y: -115)
+            //cart lock
+            VStack(alignment: .leading){
+                Text("Friends List")
+                HStack{
+                    Image("profile_fayfay").resizable().frame(width: 75, height: 75)
+                        .clipShape(Circle())
+                        .overlay{
+                            Circle().stroke(.gray, lineWidth: 1)
+                        }
+                    HStack(){
+                        VStack{
+                            Text("Nickname")
+                            Text("Fullname")
+                        }
+                        VStack{
+                                Text("Phone Number")
+                                Text("Birthdate")
+                        }.frame(maxWidth: 100, maxHeight: 100)
+                            .background(Color(red: 254/255, green: 219/255, blue: 165/255))
                     }
-                }
-                .onDelete(perform: deleteItems)
+                }.background(Color(red: 255/255, green: 143/255, blue: 145/255)).cornerRadius(10)
+                    .shadow(color: Color(red: 220/255, green: 220/255, blue: 220/255), radius: 10,x: 3, y: 0)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+            
+            //cart unlock
+            VStack(alignment: .leading){
+                HStack{
+                    Image("profile_fayfay").resizable().frame(width: 55, height: 55)
+                        .clipShape(Circle())
+                        .overlay{
+                            Circle().stroke(.gray, lineWidth: 1)
+                        }
+                    HStack(){
+                        VStack{
+                            Text("Nickname").foregroundColor(.white).bold().font(.system(size: 16, design: .rounded))
+                            Text("Fullname").foregroundColor(.white).font(.system(size: 14, design: .rounded))
+                        }
+                        VStack{
+                                Text("Phone Number").foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255)).font(.system(size: 14, design: .rounded))
+                                Text("Birthdate").foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255)).font(.system(size: 14, design: .rounded))
+                        }.frame(maxWidth: 100, maxHeight: 100)
+                            .background(Color.white)
                     }
-                }
+                }.background(Color(red: 49/255, green: 175/255, blue: 171/255)).cornerRadius(10)
+                    .shadow(color: Color(red: 220/255, green: 220/255, blue: 220/255), radius: 10,x: 3, y: 0)
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            Spacer()
+            
+            
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
+
