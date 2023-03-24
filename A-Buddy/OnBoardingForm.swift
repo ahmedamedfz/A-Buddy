24b16dfc7406122934ec193cba45959f61637e36
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2023-03-24 18:44:30 +0700

    OnBoardingForm Revisi

--- A-Buddy/OnBoardingForm.swift
+++ A-Buddy/OnBoardingForm.swift
@@ -13,61 +13,55 @@ struct OnBoardingForm: View {
     var tests = ["Select Name", "Yovita Handayiani", "me", "mine", "you", "your", "yours", "our", "ours"]
     
     var body: some View {
-        VStack{
-            ZStack{
-                
-                ZStack{
-                    
-                    Form(){
-                        Section(header:  Image("FormHeader").resizable().frame(width: 166, height: 29).padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 0))){
-                            Text("Who are you on this list?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
-
-                            Picker(selection: $selection, label: EmptyView()){
-                                ForEach(0..<tests.count) {
-                                    Text(self.tests[$0])
-                                }
-                            }.pickerStyle(.menu).tint(Color(red: 49/255, green: 175/255, blue: 171/255)).font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
-                                    .frame(width: 303,height: 1)
-                                .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)})
-                            Text("What should we call you?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
-
-                            TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
-                                
-                            }.font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255)) .overlay(VStack{Divider()
-                                    .frame(width: 303,height: 1)
-                                    .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)})
-                            Text("When is your Birthday?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
-                            TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
-                                
-                            }.font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
-                                    .frame(width: 303,height: 1)
-                                .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)}).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))
-                            Text("Your WhatsApp number?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
-                            TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
-                              
-                            }.font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
-                                    .frame(width: 303,height: 1)
-                                    .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)}).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))
-                        }
-                    }.scrollContentBackground(.hidden)
-
-                    Button {
-                        print("test")
-                    } label: {
-                        Image("PrevButton").resizable().frame(width: 43, height: 43)
-                    }.offset(x: 80, y:200)
-
-                    Button {
-                        print("test")
-                    } label: {
-                        Image("NextButton").resizable().frame(width: 43, height: 43)
-                    }.offset(x: 140, y:200)
-                }
+        ZStack{
+            VStack{
+                Form(){
+                    Section(header:  Image("FormHeader").resizable().frame(width: 166, height: 29).padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 0))){
+                        Text("Who are you on this list?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
+                        
+                        Picker(selection: $selection, label: EmptyView()){
+                            ForEach(0..<tests.count, id: \.self) {
+                                Text(self.tests[$0])
+                            }
+                        }.pickerStyle(.menu).tint(Color(red: 49/255, green: 175/255, blue: 171/255)).font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
+                                .frame(width: 303,height: 1)
+                            .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)})
+                        Text("What should we call you?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
+                        
+                        TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
+                            
+                        }.font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255)) .overlay(VStack{Divider()
+                                .frame(width: 303,height: 1)
+                            .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)})
+                        Text("When is your Birthday?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
+                        TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
+                            
+                        }.font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
+                                .frame(width: 303,height: 1)
+                            .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)}).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))
+                        Text("Your WhatsApp number?").font(.system(size: 17, design: .rounded)).listRowSeparator(.hidden)
+                        TextField(text: $test, prompt: Text("Required").font(.system(size: 17, design: .rounded)).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))){
+                            
+                        }.font(.system(size: 17, design: .rounded)).overlay(VStack{Divider()
+                                .frame(width: 303,height: 1)
+                            .background(Color(red: 49/255, green: 175/255, blue: 171/255)).offset(x: 7, y: 15)}).foregroundColor(Color(red: 49/255, green: 175/255, blue: 171/255))
+                    }
+                }.scrollContentBackground(.hidden)
             }.frame(width: 360, height: 486)
                 .background(Color.white).cornerRadius(42)
-        }
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        .background(Color(red: 254/255, green: 219/255, blue: 165/255))
+            Button {
+                print("test")
+            } label: {
+                Image("PrevButton").resizable().frame(width: 43, height: 43)
+            }.offset(x: 80, y:200)
+            
+            Button {
+                print("test")
+            } label: {
+                Image("NextButton").resizable().frame(width: 43, height: 43)
+            }.offset(x: 140, y:200)
+        } .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .background(Color(red: 254/255, green: 219/255, blue: 165/255))
     }
 }
 
